3109674596b2933ff227734894b86850b5aac2be
ajinkyawankhede30 <ajinkya_wankhede@outlook.com>
2024-06-04 22:49:48 +0530

    Add project files.

---
+++ Strategy_DPattern/IPaymentStrategy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_DPattern
+{
+    /// <summary>
+    /// 1. Create Strategy interface
+    /// </summary>
+    public interface IPaymentStrategy
+    {
+        void ProcessPayment(double amount);
+    }
+
+    /// <summary>
+    /// Define Credit card payment algorithm
+    /// </summary>
+    public class CreditCardPayment : IPaymentStrategy
+    {
+        public void ProcessPayment(double amount)
+        {
+            Console.WriteLine("Processing payment of $" + amount + " via credit card...");
+        }
+    }
+
+    /// <summary>
+    /// Define debit card payment algorithm
+    /// </summary>
+    public class DebitCardPayment : IPaymentStrategy
+    {
+        public void ProcessPayment(double amount)
+        {
+            Console.WriteLine("Processing payment of $" + amount + " via debit card...");
+        }
+    }
+
+    /// <summary>
+    /// Define UPI payment algorithm
+    /// </summary>
+    public class UPIPayment : IPaymentStrategy
+    {
+        public void ProcessPayment(double amount)
+        {
+            Console.WriteLine("Processing payment of $" + amount + " via upi...");
+        }
+    }
+
+    /// <summary>
+    /// Class to call the respective algorithm using iterface
+    /// </summary>
+    public class PaymentService
+    {
+        IPaymentStrategy iPaymentStrategy;
+        public PaymentService(IPaymentStrategy _PaymentStrategy)
+        {
+            iPaymentStrategy = _PaymentStrategy;
+        }
+
+        //Implement the respective algorithm
+        public void Payment(double amount)
+        {
+            Console.WriteLine("\nPayment service pre logic");
+            iPaymentStrategy.ProcessPayment(amount);
+            Console.WriteLine("Payment service post logic\n");
+        }
+    }
+}
---
+++ Strategy_DPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_DPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCardPayment creditCardPayment = new CreditCardPayment();
+            DebitCardPayment debitCardPayment = new DebitCardPayment();
+            UPIPayment upiPayment = new UPIPayment();
+
+            PaymentService credit_card_service = new PaymentService(creditCardPayment);
+            credit_card_service.Payment(1000.25);
+
+            PaymentService debit_card_service = new PaymentService(debitCardPayment);
+            debit_card_service.Payment(5000.12);
+
+            PaymentService upi_service = new PaymentService(upiPayment);
+            upi_service.Payment(2000.26);
+
+            Console.ReadLine();
+        }
+    }
+}
